b9b0468655b98a4871ff381f93e6df6f103da319
Dejan Budimir <dejbug@users.noreply.github.com>
2023-06-04 16:07:18 +0200

    shebang

--- src/tools/scheine.sh
+++ src/tools/scheine.sh
@@ -1,2 +1,3 @@
+#!/bin/bash
 [[ ! -a "scheine.json" ]] && curl -skL -o scheine.json "https://scheinefuervereine.rewe.de/consumer-api/customer.php?action=get_club&id=10000037078";
 [[ -a "scheine.json" ]] && sed -n 's/.*,"totalBalance":\([^,]*\).*/\1/p' scheine.json
