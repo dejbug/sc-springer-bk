e84ed9439be61005d02f2d5ec211a3ef84748f47
Dejan Budimir <dejbug@users.noreply.github.com>
2024-07-23 23:41:57 +0200

    workaround

--- src/main.mk
+++ src/main.mk
@@ -11,15 +11,24 @@ MISC += $(wildcard vendor/rewe/img/*.png)
 endif
 MISC := $(MISC:%=dist/%)
 
-# VEREINSTURNIERE := $(shell python tools/vereinsturniere.py --tdir tables --list)
-# VEREINSTURNIERE := $(VEREINSTURNIERE:%=dist/vereinsturniere-%.html)
-# VEREINSTURNIERE := $(patsubst %,dist/vereinsturniere-%.html,$(VEREINSTURNIERE))
-# VEREINSTURNIERE := $(shell python tools/patsubst.py \(.+\) dist/vereinsturniere-\\1.html '22 23 24')
 # HACK. Something's wrong with the deployment server's handling of makefiles. Something
 #	with the piping sends debug output through our $(shell python ...) call.
+
+# This used to work remotely. Locally of course it works.
+# VEREINSTURNIERE := $(shell python tools/vereinsturniere.py --tdir tables --list)
+# VEREINSTURNIERE := $(VEREINSTURNIERE:%=dist/vereinsturniere-%.html)
+
+# None of these work (remotely):
+# VEREINSTURNIERE := $(patsubst %,dist/vereinsturniere-%.html,$(VEREINSTURNIERE))
+# VEREINSTURNIERE := $(shell python tools/patsubst.py \(.+\) dist/vereinsturniere-\\1.html '22 23 24')
 # VEREINSTURNIERE := $(shell python tools/vereinsturniere.py --list)
 # VEREINSTURNIERE := $(shell python -c 'print(22, 23, 24)')
-VEREINSTURNIERE := $(shell echo 22 23 24)
+
+# This works, trivially.
+# VEREINSTURNIERE := $(shell echo 22 23 24)
+
+# Temporary workaround. (Issue was filed with our static site deployment host.)
+VEREINSTURNIERE := 22 23 24
 VEREINSTURNIERE := $(VEREINSTURNIERE:%=dist/vereinsturniere-%.html)
 
 HTML := $(wildcard *.html) default.css
