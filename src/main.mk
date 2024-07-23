d220719d53c0822e2b855613b2363fea6ee7c1e1
Dejan Budimir <dejbug@users.noreply.github.com>
2024-07-23 23:03:36 +0200

    -

--- src/main.mk
+++ src/main.mk
@@ -11,12 +11,13 @@ MISC += $(wildcard vendor/rewe/img/*.png)
 endif
 MISC := $(MISC:%=dist/%)
 
-# VEREINSTURNIERE := dist/vereinsturniere-24.html dist/vereinsturniere-22.html dist/vereinsturniere-23.html
-VEREINSTURNIERE = $(shell "python tools/vereinsturniere.py --tdir tables --list")
+# VEREINSTURNIERE := $(shell python tools/vereinsturniere.py --tdir tables --list)
 # VEREINSTURNIERE := $(VEREINSTURNIERE:%=dist/vereinsturniere-%.html)
 # VEREINSTURNIERE := $(patsubst %,dist/vereinsturniere-%.html,$(VEREINSTURNIERE))
 # VEREINSTURNIERE := $(shell python tools/patsubst.py \(.+\) dist/vereinsturniere-\\1.html '22 23 24')
-# VEREINSTURNIERE := 22 23 24
+# HACK. Something's wrong with the deployment server's handling of makefiles. Something
+#	with the piping sends debug output through our $(shell)
+VEREINSTURNIERE := $(shell echo 22 23 24)
 VEREINSTURNIERE := $(VEREINSTURNIERE:%=dist/vereinsturniere-%.html)
 
 HTML := $(wildcard *.html) default.css
